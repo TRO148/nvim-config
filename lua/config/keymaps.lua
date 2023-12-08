88d7304aa5e50193aad12dff5862c7a00d8c80c8
tro148 <1481895142@qq.com>
2023-12-08 13:01:53 +0800

    更新配置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- 机器人
+vim.keymap.set("n", "<Leader>co", ":Copilot enable<CR>")
+vim.keymap.set("n", "<Leader>cO", ":Copilot disable<CR>")
